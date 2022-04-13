dc6f823d55aa8e9c2bdefaadc3b69f411ca7ee8d
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-04-13 19:19:08 +0430

    codeforces: Ilya_and_queries

---
+++ codeforces/Ilya_and_queries.py
@@ -0,0 +1,11 @@
+a = input().strip()
+dp = [0, 0]
+for i in range(1, len(a)):
+    if a[i] == a[i - 1]:
+        dp.append(dp[i] + 1)
+    else:
+        dp.append(dp[i])
+n = int(input())
+for _ in range(n):
+    i, j = map(int, input().split())
+    print(dp[j] - dp[i])

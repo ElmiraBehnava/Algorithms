88fd5ab26fe0f91a483b5bc4edf1e48502d01922
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-04-13 19:33:10 +0430

    codeforces: add document to make_product_equal_one

--- codeforces/make_product_equal_one.py
+++ codeforces/make_product_equal_one.py
@@ -10,3 +10,9 @@ for i in range(1, len(a)):
         min(dp_m[i - 1] + abs(a[i] - 1), dp_plus[i - 1] + abs(a[i] + 1))
     )
 print(dp_plus[-1])
+
+"""
+    dp_plus[i] = min(dp_plus[i - 1] + abs(a[i] - 1), dp_m[i - 1] + abs(a[i] + 1))
+    dp_minus[i] = min(dp_minus[i - 1] + abs(a[i] + 1), dp_plus[i - 1] + abs(a[i] - 1))
+    
+"""

585a83bd68e8993bc4685dc3e96cb487236406bf
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-04-13 19:17:15 +0430

    make_product_equal_one codeforces

---
+++ codeforces/make_product_equal_one.py
@@ -0,0 +1,12 @@
+k = int(input())
+a = list(map(int, input().split()))
+dp_plus = [abs(a[0] - 1)]
+dp_m = [abs(a[0] + 1)]
+for i in range(1, len(a)):
+    dp_plus.append(
+        min(dp_plus[i - 1] + abs(a[i] - 1), dp_m[i - 1] + abs(a[i] + 1))
+    )
+    dp_m.append(
+        min(dp_m[i - 1] + abs(a[i] - 1), dp_plus[i - 1] + abs(a[i] + 1))
+    )
+print(dp_plus[-1])

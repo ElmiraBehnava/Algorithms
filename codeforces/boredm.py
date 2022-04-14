cf0a899d238c3ac418983b344cf61a189a14ffcb
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-04-14 15:40:24 +0430

    codeforces: bordem

---
+++ codeforces/boredm.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+k = int(input())
+n = list(map(int, input().split(" ")))
+d = defaultdict(int)
+dp = [0]
+for i in n:
+    d[i] += 1
+for i in range(1, max(n) + 1):
+    dp.append(max(dp[i - 1], (i * d[i]) + dp[i - 2]))
+print(dp[max(n)])
